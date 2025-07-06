fdf905f98acd6c2b3f12a62c1e36db176209e08c
unknown <manshibharti9546@gmail.com>
2025-07-06 12:06:48 +0530

    opdt

--- counter.js
+++ counter.js
@@ -7,3 +7,6 @@ export function setupCounter(element) {
     element.addEventListener('click', () => setCounter(counter + 1))
     setCounter(0)
   }
+  document.querySelector('.hamburger-menu').addEventListener('click', () => {
+  document.querySelector('.mobile-nav').classList.toggle('show');
+});
